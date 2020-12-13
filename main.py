6778c975f6e73e26d5be81da40c88ed5f0d3e5e8
Your Name <baobab744@yandex.ru>
2020-12-13 19:17:31 +1000

    1

--- main.py
+++ main.py
@@ -1,48 +1,55 @@
-from tkinter import Tk, ttk, Entry, INSERT, END, W, E, Label
-import random
+from tkinter import *
+from math import sqrt
 
 
-def start():
-    a = random.randint(1, 99)
-    b = random.randint(1, 99)
-    c = 0
-    result = a + b
-
-    lab1['text'] = f'Score:{c}'
-    lab2['text'] = f'{a} + {b}'
-
-    block1.delete(0, END)
-
-
-def check():
-    num = block1.get()
-    if num == result:
-        lab1['text'] = f'Score:{c + 1}'
-        lab2['text'] = f'{a} + {b}'
+def solve(a, b, c):
+    D = b * b - 4 * a * c
+    if D >= 0:
+        x1 = (-b + sqrt(D) // (2 * a))
+        x2 = (-b - sqrt(D) // (2 * a))
+        text = 'Дискриминант уравнения D = {D}, X1 = {x1}, X2 = {x2}.'
     else:
-        lab2['text'] = f'Game Over, Try Again'
+        text = 'Дискриминант уравнения D = {D}, корней не существует.'
+
+
+def inserter(value):
+    output.delete('1.0', END)
+    output.insert(1.0, value)
+
+
+def main():
+    try:
+        val1 = float(entry1.get())
+        val2 = float(entry2.get())
+        val3 = float(entry3.get())
+        inserter(solve(val1, val2, val3))
+    except ValueError:
+        inserter('Впишите числа в каждое из трёх полей!')
 
 
 root = Tk()
-root.title('Calcul to Win')
+root.title('Калькулятор квадратных уравнений')
+root.minsize(640,480)
+root.resizable(width=False, height=False)
 
-style = ttk.Style()
-style.configure('TButton', background='white', foreground='black', font='Arial 15')
+entry1 = Entry(root, font='Arial 16', width=5)
+entry1.grid(row=1, column=1, ipady=10)
 
-lab1 = Label(root, font='Arial 30', state='normal', fg='black')
-lab1.grid(row=0, column=0, columnspan=2, sticky=W + E, ipady=20)
+lab1 = Label(root, font='Arial 24', text='x² + ').grid(row=1, column=2, ipady=20)
 
-lab2 = Label(root, font='Arial 30', state='normal', fg='black')
-lab2.grid(row=1, column=0, columnspan=2, sticky = W + E, ipady=20)
+entry2 = Entry(root, font='Arial 16', width=5)
+entry2.grid(row=1, column=3, ipady=10)
 
-block1 = Entry(root, font='Arial 30', insertontime=0, relief='solid', state='normal', fg='black')
-block1.focus()
-block1.grid(row=2, column=0, columnspan=2, sticky=W + E, ipady=20)
+lab2 = Label(root, font='Arial 24', text='x + ').grid(row=1, column=4)
+ 
+entry3 = Entry(root, font='Arial 16', width=5)
+entry3.grid(row=1, column=5, ipady=10)
 
-button1 = ttk.Button(root, text='Enter', command=check)
-button1.grid(row=3, column=0, ipady=10)
+lab3 = Label(root, font='Arial 24', text='= 0').grid(row=1, column=6)
 
-button2 = ttk.Button(root, text='Start/Play Again', command=start)
-button2.grid(row=3, column=1, ipady=10)
+btn = Button(root, font='Arial 16', text='Решить', command=main).grid(row=1, column=7, ipady=10)
+
+output = Text(root, bg='lightblue', fg='navy', font='Arial 20', width=45, height=13, wrap='n')
+output.grid(row=2, columnspan=8)
 
 root.mainloop()
